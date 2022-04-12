9b7237287aa8eac9dd63715a542df97e0e6ca89e
ayerst9198 <78511779+ayerst9198@users.noreply.github.com>
2022-04-13 09:45:38 +1200

    created sandbox and base component

---
+++ 00_Sandbox.py
@@ -0,0 +1,11 @@
+import time
+
+def ester_eg():
+    print("Bill Nye the Science Guy")
+    time.sleep(2)
+    print("*billl nyeee the scienceee guyyyyy*")
+    print()
+    time.sleep(3.45)
+    print("*Bill* *Bill* *Bill* *Bill* *Bill* *Bill* *Bill* *Bill* *Bill* ")
+
+ester_eg()
