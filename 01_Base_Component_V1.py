71e4ea3d5797b5b7484bc470dd8d802c91dfb3e4
ayerst9198 <78511779+ayerst9198@users.noreply.github.com>
2022-04-14 13:53:28 +1200

    Update 01_Base_Component_V1.py

--- 01_Base_Component_V1.py
+++ 01_Base_Component_V1.py
@@ -1,3 +1,28 @@
 # Functions go here
 
 # Main routine goes here
+
+# lists
+
+# instructions
+
+# start loop
+
+# ask user if they want to incrypt or decrypt
+
+# if user wants to decrypt:
+# ask user for the text (if xxx exit loop)
+# ask user if they know the key
+# if user doesn't know the key, decrypt without key
+# if user does know key, decrypt with key
+
+# if user wants to encrypt
+# ask user for text to encrypt (if xxx exit loop)
+# ask if the user wants to enter the key
+# if user says yes, allow them to enter the key and then encrypt using key
+# if user says no, randomly generate key and encrypt using key 
+
+# end loop
+
+# write data to text file
+
