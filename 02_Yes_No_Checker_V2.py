6c41012f17c001b6218307b671ef8bf73f391853
ayerst9198 <78511779+ayerst9198@users.noreply.github.com>
2022-05-10 10:48:41 +1200

    Update 02_Yes_No_Checker_V2.py

--- 02_Yes_No_Checker_V2.py
+++ 02_Yes_No_Checker_V2.py
@@ -1,32 +1,53 @@
-# functions go here
+# string checker
+def string_checker(choice, options):
+    for var_list in options:
 
-# yes no checker pulled from frc
-def yes_no(question):
-    valid = False
-    while not valid:
-        response = input(question).lower()
+        # if snack is in list return full snack name
+        if choice in var_list:
 
-        if response == "yes" or response == "y":
-            response = "yes"
-            return response
-
-        elif response == "no" or response == "n":
-            response = "no"
-            return response
-
+            # get full name of snack and put it 
+            # in title case so it looks noice
+            chosen = var_list[0].title()
+            is_valid = "yes"
+            break
+        # if choe option is not valid, set is_valid to no
         else:
-            print("Please input yes / no")
-            print()
+            is_valid = "no"
+    
+    # if snack is not ok - ask again
+    if is_valid == "yes":
+        return chosen
+    else: 
+        return "invalid choice"
 
-# main routin
 
-#loop for testing
+# answer list
+yes_no = [
+    ["yes", "y"],
+    ["no", "n"]
+]
+# looped for testing
 while 1 == 1:
-    yesno = yes_no("Would you like to read the instructions? ")
-    print()
-    if yesno == "yes":
-        print("instructions go here")
-        print("program continues")
+    
+    # define variable
+    ans = "invalid choice"
+    while ans == "invalid choice":
+
+        # ask user hte question
+        ans = input("Did Toby legit beat Ryan in cr random deck 1v1? ")
+
+        # checks if answer is valid
+        ans = string_checker(ans, yes_no)
+
+        # loops if invalid option
+        if ans == "invalid choice":
+            print("Please enter yes or no")
+
+    # prints our responbse based on answer
+    if ans == "Yes":
+        print("correct")
+        print()
     else:
-        print("program continues")
-    print()
+        print("wrong")
+        print()
+
